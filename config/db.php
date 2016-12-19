9f0a1f943ccc5b75d519226774c9a03feeca5174
harold <haroldfolk@gmail.com>
2016-12-19 15:25:19 -0400

    cambios a digital

--- config/db.php
+++ config/db.php
@@ -1,23 +1,10 @@
 <?php
 
-//return [
-//    'class' => 'yii\db\Connection',
-//    'dsn' => 'pgsql:host=200.87.228.58;dbname=db_grupo05sa',
-//    'username' => 'grupo05sa',
-//    'password' => 'grupo05grupo05',
-//    'charset' => 'utf8',
-//    'schemaMap' => [
-//        'pgsql' => [
-//            'class' => 'yii\db\pgsql\Schema',
-//            'defaultSchema' => 'public' //specify your schema here
-//        ]
-//    ], // PostgreSQL
-//];
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=postgres',
-    'username' => 'postgres',
-    'password' => 'nm',
+    'dsn' => 'pgsql:host=200.87.228.58;dbname=db_grupo05sa',
+    'username' => 'grupo05sa',
+    'password' => 'grupo05grupo05',
     'charset' => 'utf8',
     'schemaMap' => [
         'pgsql' => [
@@ -26,3 +13,16 @@ return [
         ]
     ], // PostgreSQL
 ];
+//return [
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'pgsql:host=localhost;dbname=postgres',
+//    'username' => 'postgres',
+//    'password' => 'nm',
+//    'charset' => 'utf8',
+//    'schemaMap' => [
+//        'pgsql' => [
+//            'class' => 'yii\db\pgsql\Schema',
+//            'defaultSchema' => 'public' //specify your schema here
+//        ]
+//    ], // PostgreSQL
+//];
